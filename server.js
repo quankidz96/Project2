cd1ae385dad34bc82078b0949e3a88fd2250d248
Quan <quan.nh148983@sis.hust.edu.vn>
2020-05-04 00:28:23 +0700

    Khơi tạo một số thứ + code header cho web

---
+++ server.js
@@ -0,0 +1,25 @@
+// .ENV 
+require('dotenv').config();
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT;
+
+// USE template engines PUG
+app.set('view engine', 'pug');
+
+// SET views to render
+app.set('views', './views');
+
+//static file
+app.use(express.static('public'));
+
+// listen port
+app.listen(port, function(){
+    console.log('Server Starting on port: ' + port);
+    
+});
+
+app.get('/', function(req, res){
+    res.render('index');
+});
