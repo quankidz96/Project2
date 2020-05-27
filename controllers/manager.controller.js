e44832e77c386c9f43e7749c26ca1dba14e9595d
Quan <quan.nh148983@sis.hust.edu.vn>
2020-05-28 00:03:25 +0700

    COde phan dang nhap cho admin

--- controllers/manager.controller.js
+++ controllers/manager.controller.js
@@ -1,6 +1,6 @@
 var firebase = require('../firebase.js');
 var usersRef = firebase.db.ref('users');
-
+var bcrypt = require('bcrypt');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
@@ -13,56 +13,52 @@ module.exports.admin = function(req, res){
     res.render('admin/main/index.ejs');
 }
 
-// module.exports.authenticate = passport.authenticate(
-//     'local', { 
-//         successRedirect: '/admin',
-//         failureRedirect: '/admin/login',
-//         failureFlash: true 
-//     });
-
-// passport.use(new LocalStrategy({
-//     usernameField: 'username',
-//     passwordField: 'password'
-//     },
-//     function(username, password, done) {
-//         usersRef.once('value', function(data){
-//             var user = date.val()
-//             if(user.username === username && user.password === password ) {
-//                 return done(null, username)
-//             }
-//             else{
-//                 return done(null, false)
-//             }
-//         });
-//       User.findOne({email: username}, function(err, username){
-//           if(err) throw err;
-//           if(username){
-//             bcrypt.compare(password, username.password, function(err, user) {
-//                 if(err) throw err;
-//                 if(user){
-//                      return done(null, username);
-//                 }else{
-//                    return done(null, false, { message: 'Tài Khoảng Không Đúng' });
-//                 }
-//             });
-//           }else{
-//              return done(null, false, { message: 'Tài Khoảng Không Đúng' });
-//           }
-//       });
-//     }
-// ));
-
-// passport.serializeUser(function(email, done) {
-    
-//     done(null, email.id);
-// });
-
-// passport.deserializeUser(function(id, done) {
-//     User.findById(id, function(err, email) {
-//     done(err, email);
-//     });
-// });
-
-module.exports.isAuthenticated = function(req, res, next){
-    
+module.exports.logout = function(req, res){
+    req.logout();
+    res.redirect('/admin/login');
 }
+
+module.exports.authenticate = passport.authenticate(
+    'local', { 
+        successRedirect: '/admin',
+        failureRedirect: '/admin/login',
+        failureFlash: true 
+    });
+
+passport.use(new LocalStrategy(
+    function(username, password, done){ 
+        usersRef.once('value', function(data){
+            if(username == data.val().username) { 
+                bcrypt.compare(password, data.val().password, function(err, result) {
+                    if(result){
+                        return done(null, username);
+                    }else{
+                        console.log('password sai ')
+                        return done(null, false);
+                    }
+                });
+            } else {
+                console.log('Tai khoan ko ton tai')
+                return done(null, false); 
+            }
+        });
+    }
+));
+
+passport.serializeUser(function(username, done){
+    usersRef.once('value', function(data){
+        done(null, data.val().id);
+    })
+});
+
+passport.deserializeUser(function(id, done){
+    usersRef.once('value', function(data){
+        if (id === data.val().id) { 
+            return done(null, data.val().id)
+        } else {
+            return done(null, false)
+        }
+    })
+    
+});
+
