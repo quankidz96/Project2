1bc946ddb40da88de4527c986e28ace8c4da3389
Quan <quan.nh148983@sis.hust.edu.vn>
2020-05-27 21:05:02 +0700

    update

--- controllers/manager.controller.js
+++ controllers/manager.controller.js
@@ -1,9 +1,68 @@
+var firebase = require('../firebase.js');
+var usersRef = firebase.db.ref('users');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
 module.exports.login = function(req, res) {
-    res.render('admin/login/login.ejs');
+    res.render('admin/login/index.ejs');
 }
+
 module.exports.admin = function(req, res){
     res.render('admin/main/index.ejs');
 }
+
+// module.exports.authenticate = passport.authenticate(
+//     'local', { 
+//         successRedirect: '/admin',
+//         failureRedirect: '/admin/login',
+//         failureFlash: true 
+//     });
+
+// passport.use(new LocalStrategy({
+//     usernameField: 'username',
+//     passwordField: 'password'
+//     },
+//     function(username, password, done) {
+//         usersRef.once('value', function(data){
+//             var user = date.val()
+//             if(user.username === username && user.password === password ) {
+//                 return done(null, username)
+//             }
+//             else{
+//                 return done(null, false)
+//             }
+//         });
+//       User.findOne({email: username}, function(err, username){
+//           if(err) throw err;
+//           if(username){
+//             bcrypt.compare(password, username.password, function(err, user) {
+//                 if(err) throw err;
+//                 if(user){
+//                      return done(null, username);
+//                 }else{
+//                    return done(null, false, { message: 'Tài Khoảng Không Đúng' });
+//                 }
+//             });
+//           }else{
+//              return done(null, false, { message: 'Tài Khoảng Không Đúng' });
+//           }
+//       });
+//     }
+// ));
+
+// passport.serializeUser(function(email, done) {
+    
+//     done(null, email.id);
+// });
+
+// passport.deserializeUser(function(id, done) {
+//     User.findById(id, function(err, email) {
+//     done(err, email);
+//     });
+// });
+
 module.exports.isAuthenticated = function(req, res, next){
     
 }
--- public/js/home.js
+++ public/js/home.js
@@ -2,12 +2,22 @@
 function pointThousand(){
 var regex = /\B(?=(\d{3})+(?!\d))/g;
     var prices = document.getElementsByClassName("pricenew");
-    for(price of prices){
-        console.log(price);
-        
-        //var intprice = parseInt(price.textContent, 10);
+    for(price of prices){   
         var newprice = price.textContent.toString().replace(regex, ".") + '   VND';
         price.innerHTML = newprice;
     };
 }
 pointThousand();
+
+function deleteEmptyCate (){
+    var containers = document.getElementsByClassName('productcontainer');
+    var cates = document.getElementsByClassName('cates');
+    var i = -1;
+    for(container of containers){
+        i++;
+        if(!container.childElementCount){
+            cates[i].setAttribute('style', 'display: none; visibility: hidden');
+        }
+    }
+}
+deleteEmptyCate();
